15bf3f8c0376fe27fe3d6ec10357b1b7b726dc54
GiraudonMathilde <mathilde.giraudon@gmail.com>
2021-03-20 18:26:21 +0100

    front: add styles on _app.js + test on projet

--- front-end/pages/projet.js
+++ front-end/pages/projet.js
@@ -2,6 +2,7 @@ import Layout from '../components/Layout'
 import Head from 'next/head'
 
 const Projet = () => {
+  
   return (
     <>
       <Head>
@@ -10,6 +11,23 @@ const Projet = () => {
 
       <Layout>
         <h1>Le projet</h1>
+        <h2>Rejoint une AMAP !</h2>
+        <div className = 'paragraph'>
+          <p>Pollution. <br />
+          Gâchis. <br />
+          Suremballage. <br />
+          Dégradation des sols et des paysages.<br /> 
+          Scandales sanitaires.<br />
+          Pesticides. <br />
+          Misère et suicides des paysans… <br />
+          Le système agroalimentaire actuelle ne te correspond plus 
+          et tu veux à ton échelle changer leurs choses ?
+          </p>
+          <img src="/projet/recolteBle.jpg" width='30%' />
+        </div>
+  
+        
+        
       </Layout>
     </>
   )
