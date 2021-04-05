c96cbc1debe4689e93bc88623d47605af75f7d58
Paul Farault <paul.farault@gmail.com>
2021-04-05 22:07:20 +0200

    front: creation form in component

---
+++ front-end/components/CreationForm.jsx
@@ -0,0 +1,95 @@
+import { useState } from 'react'
+// Material ui
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography'
+
+export default () => {
+
+  const [name, setName] = useState("")
+  const [address, setAddress] = useState("")
+  const [lat, setLat] = useState("")
+  const [long, setLong] = useState("")
+  const [phone, setPhone] = useState("")
+  const [email, setEmail] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+  }
+
+  return (
+    <>
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <Paper style={{ padding: 16 }}>
+          <Grid container alignItems="flex-start" spacing={2}>
+            <Typography variant="h3" align="center">Mon AMAP</Typography>
+            <Grid item xs={12}>
+              <TextField
+                id="name"
+                label="Nom de l'AMAP"
+                fullWidth
+                value={name}
+                onChange={e => setName(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                id="address"
+                label="Adresse"
+                fullWidth
+                value={address}
+                onChange={e => setAddress(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                id="lat"
+                label="Latitude"
+                fullWidth
+                value={lat}
+                onChange={e => setLat(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                id="long"
+                label="Longitude"
+                fullWidth
+                value={long}
+                onChange={e => setLong(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                id="phone"
+                label="Numéro de téléphone"
+                fullWidth
+                value={phone}
+                onChange={e => setPhone(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                id="email"
+                label="Adresse email"
+                fullWidth
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+            </Grid>
+            <Button
+              variant="contained"
+              type="submit"
+              color="primary"
+            >
+              Ajouter mon AMAP
+              </Button>
+          </Grid>
+        </Paper>
+      </form>
+    </>
+  )
+}
--- front-end/pages/nouvelle.js
+++ front-end/pages/nouvelle.js
@@ -1,11 +1,7 @@
-import Layout from '../components/Layout'
 import Head from 'next/head'
-// Material ui
-import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography'
+
+import Layout from '../components/Layout'
+import CreationForm from '../components/CreationForm'
 
 const Nouvelle = () => {
 
@@ -20,32 +16,8 @@ const Nouvelle = () => {
           <h1>CREATION NOUVELLE AMAP</h1>
         </header>
 
-        <form autoComplete="off">
-          <Paper style={{ padding: 16 }}>
-            <Grid container alignItems="flex-start" spacing={2}>
-              <Typography variant="h3" align="center">Mon AMAP</Typography>
-              <Grid item xs={12}>
-                <TextField id="name" label="Nom de l'AMAP" fullWidth />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField id="address" label="Adresse" fullWidth />
-              </Grid>
-              <Grid item xs={6}>
-                <TextField id="lat" label="Latitude" fullWidth />
-              </Grid>
-              <Grid item xs={6}>
-                <TextField id="long" label="Longitude" fullWidth />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField id="phone" label="Numéro de téléphone" fullWidth />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField id="email" label="Adresse email" fullWidth />
-              </Grid>
-              <Button variant="contained" type="submit" color="primary">Ajouter mon AMAP</Button>
-            </Grid>
-          </Paper>
-        </form>
+        <CreationForm />
+
       </Layout>
     </>
   )
