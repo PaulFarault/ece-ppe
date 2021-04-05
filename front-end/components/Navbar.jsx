9b978e11f9cebb9a1019b37730ceff286f0f6f64
Paul Farault <paul.farault@gmail.com>
2021-04-05 21:39:42 +0200

    front: navbar style

--- front-end/components/Navbar.jsx
+++ front-end/components/Navbar.jsx
@@ -1,35 +1,27 @@
 import Link from 'next/link'
+import { useRouter } from "next/router";
+
+import styles from './Navbar.module.scss'
 
 export default () => {
+
+  const router = useRouter();
+
   return (
     <>
-      <nav >
-        <ul >
-          <li >
-            <Link href="/">
-            <a>Accueil</a>
-            </Link>
-          </li>
-          <li >
-            <Link href="/carte">
-            <a>Carte</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/projet">
-            <a>Projet</a>
-            </Link>
-          </li>
-          <li>
-          <Link href="/nouvelle">
-          <a>Creation AMAP</a>
-          </Link>
-          </li>
-        </ul>
-        
-        
-        
-        
+      <nav className={styles.nav}>
+        <Link href="/">
+          <a className={router.pathname == "/" ? "active" : ""}>Accueil</a>
+        </Link>
+        <Link href="/carte">
+          <a className={router.pathname == "/carte" ? "active" : ""}>Carte</a>
+        </Link>
+        <Link href="/projet">
+          <a className={router.pathname == "/projet" ? "active" : ""}>Projet</a>
+        </Link>
+        <Link href="/nouvelle">
+          <a className={router.pathname == "/nouvelle" ? "active" : ""}>Creation AMAP</a>
+        </Link>
       </nav>
     </>
   )
