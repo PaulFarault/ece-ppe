fc63f8211cbb9537d15d7848094e4123be5bb110
Paul Farault <paul.farault@gmail.com>
2021-04-05 22:26:53 +0200

    back: add cors

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
 
 module.exports = app
   .use(express.json()) // => req.body
+  .use(cors())
   .get('/', (ignore, res) => { res.send('<h1>Back-end</h1>') })
   .use('/', routes)
