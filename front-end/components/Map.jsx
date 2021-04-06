4452a87a6bbd38f1d78cbc18c10b7d39e8edce2a
Paul Farault <paul.farault@gmail.com>
2021-04-06 05:06:34 +0200

    front: fetch amaps + display on map

--- front-end/components/Map.jsx
+++ front-end/components/Map.jsx
@@ -1,12 +1,24 @@
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import axios from 'axios'
 import 'leaflet/dist/leaflet.css'
 import { icon } from "leaflet"
+import { useEffect, useState } from 'react'
 
-const ICON = icon({
-  iconUrl: "/map-marker-alt-solid.png",
-  iconSize: [24, 32],
-})
+const useFetch = url => {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setData(data)
+    setLoading(false)
+  }, [])
+
+  return { data, loading }
+}
+
 
 export default ({ position }) => {
   const [long, lat] = position
@@ -18,18 +30,30 @@ export default ({ position }) => {
     zoom = 6
   }
 
+  const ICON = icon({
+    iconUrl: "/map-marker-alt-solid.png",
+    iconSize: [24, 32],
+  })
+
+  const { data, loading } = useFetch(`http://localhost:3001/amap`)
+
   return (
     <MapContainer center={position} zoom={zoom} scrollWheelZoom={false} style={{ height: 400, width: "100%" }}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker
-        position={position}
-        icon={ICON}
-      >
-        <Popup>A pretty CSS3 popup. <br /> Easily customizable.</Popup>
-      </Marker>
+      {
+        !loading && data.map(amap => (
+          <Marker
+            position={[amap.latitude, amap.longitude]}
+            icon={ICON}
+          >
+            <Popup>{amap.amap_address}</Popup>
+          </Marker>
+        ))
+      }
+
     </MapContainer>
   )
 }
